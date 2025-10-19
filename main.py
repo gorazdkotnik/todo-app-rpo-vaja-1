721c87e9111fded388b09ff849c1a2929bd01999
Gorazd Kotnik <gorazdkotnik05@gmail.com>
2025-10-19 21:00:01 +0200

    Sprememba glavne veje

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from todo_app import TodoApp
 
 if __name__ == "__main__":
     print("Welcome to the Todo Application!")
+    print("To je sprememba iz main veje.")
     app = TodoApp()
     app.menu()
