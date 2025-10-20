722fee847f6190acadb09ed7ca328d56e4636fff
Gorazd Kotnik <gorazdkotnik05@gmail.com>
2025-10-20 12:26:45 +0200

    Sprememba v glavni veji (main)

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from todo_app import TodoApp
 
 if __name__ == "__main__":
     print(greet_user("User"))
+    print(subtract(5, 3))
     app = TodoApp()
     app.menu()
 
 
 def greet_user(name):
     return f"Hello, {name}! Welcome to the Todo Application!"
+
+
+def subtract(a, b):
+    return a - b
