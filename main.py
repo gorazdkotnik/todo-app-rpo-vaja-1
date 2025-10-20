7909cbae2a3846546ef0192c871754b54a7ac64c
Gorazd Kotnik <gorazdkotnik18@gmail.com>
2025-10-20 12:05:57 +0200

    Nova funkcionalnost

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from todo_app import TodoApp
 
 if __name__ == "__main__":
-    print("Welcome to the Todo Application!")
+    print(greet_user("User"))
     app = TodoApp()
     app.menu()
+
+
+def greet_user(name):
+    return f"Hello, {name}! Welcome to the Todo Application!"
